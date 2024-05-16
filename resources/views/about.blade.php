8f80d346a7f1959d1dd44a11f004c38f587e856c
Pande Yogam <pandeyogam321@gmail.com>
2024-05-16 13:04:17 +0800

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+  <x-slot:title>{{ $title }}</x-slot:title>
+  <h3 class="text-xl">Ini adalah halaman hompage</h3>
+
+  <h1>Halaman about</h1>
+
+    <img src="img/pande.jpg" alt="pande" width="200">
+</x-layout>
